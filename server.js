01d37d8bf8b921565d64907e86203300c06b43c4
Steve Rector <steverector27@gmail.com>
2024-03-26 18:05:19 -0700

    Add dotenv and env configuration

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env' });
-const { PORT } = process.env;
+dotenv.config();
+const { PORT, NODE_ENV } = process.env;
 
 const app = express();
 app.use(express.json());
 
-app.use(morgan('dev'));
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
@@ -17,5 +19,12 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.json({
     test: 'hello!',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.json({
+    message: 'data received',
+    data: req.body,
   });
 });
