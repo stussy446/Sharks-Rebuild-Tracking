7ffabd9d388f2d4554b1b8374247c65ece733d84
Steve Rector <steverector27@gmail.com>
2024-03-28 12:47:59 -0700

    commit before removing env from github

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ let { DATABASE } = process.env;
 
 DATABASE = DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DATABASE).then(() => {
-  console.log('DB Connection successful');
-});
+// Connect to mongodb or exit application if it cannot be connected to
+try {
+  mongoose.connect(DATABASE).then(() => {
+    console.log('DB Connection successful');
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
 
+// run application and listen on provided port
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
