4470d01aeffd0b677d928b8a41638cf210c36b18
Steve Rector <steverector27@gmail.com>
2024-03-28 00:27:31 -0700

    Add placeholder logic for post and get functions

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -2,10 +2,12 @@ import express from 'express';
 import {
   getAllPlayers,
   createPlayer,
+  getPlayer,
 } from '../controllers/playerController.js';
 
 const router = express.Router();
 
 router.route('/').get(getAllPlayers).post(createPlayer);
+router.route('/:id').get(getPlayer);
 
 export default router;
