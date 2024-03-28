2b7d9468422f7a641289c1ac72c4a5f2b83642d9
Steve Rector <steverector27@gmail.com>
2024-03-28 11:35:28 -0700

    Add basic functionality for all Player CRUD routes

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -64,3 +64,51 @@ export const getPlayer = async (req, res) => {
     });
   }
 };
+
+export const updatePlayer = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const { name, team, position } = req.body;
+    const foundPlayer = players.find((player) => player.id === id);
+    if (!foundPlayer) {
+      throw new Error('player not found');
+    }
+
+    (foundPlayer.name = name || foundPlayer.name),
+      (foundPlayer.team = team || foundPlayer.team),
+      (foundPlayer.position = position || foundPlayer.position),
+      res.status(201).json({
+        status: 'success',
+        foundPlayer,
+        players,
+      });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error.message,
+    });
+  }
+};
+
+export const deletePlayer = async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const foundPlayer = players.find((player) => player.id === id);
+    if (!foundPlayer) {
+      throw new Error('player not found');
+    }
+
+    players = players.filter((player) => player.id != id);
+
+    res.status(201).json({
+      status: 'success',
+      foundPlayer,
+      players,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error.message,
+    });
+  }
+};
--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -3,11 +3,13 @@ import {
   getAllPlayers,
   createPlayer,
   getPlayer,
+  updatePlayer,
+  deletePlayer,
 } from '../controllers/playerController.js';
 
 const router = express.Router();
 
 router.route('/').get(getAllPlayers).post(createPlayer);
-router.route('/:id').get(getPlayer);
+router.route('/:id').get(getPlayer).patch(updatePlayer).delete(deletePlayer);
 
 export default router;
