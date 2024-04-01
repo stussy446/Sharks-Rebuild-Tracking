21f79b1c9c8aead5cf4c06dd24128391271674f9
Steve Rector <steverector27@gmail.com>
2024-03-31 19:45:45 -0700

    Add bcrpyt hashing to passwords on user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 // **************
 // NOTE:, I am utilizing the express-async-errors package (imported in server.js) which essentially handles try catch behind the scenes,
@@ -12,11 +13,18 @@ import { StatusCodes } from 'http-status-codes';
 
 // Register
 export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  // hashes password with bcrypt
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
 
   res.status(StatusCodes.OK).json({
     status: 'success',
-    user,
+    message: 'user created',
   });
 };
 
